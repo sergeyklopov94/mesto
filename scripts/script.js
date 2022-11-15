600e36689f3503159c20e2d6803312b75ab596d3
Sergey Klopov <sergklo94@gmail.com>
2022-11-15 12:31:49 +0300

    feat: создать функционал автозаполнения полей ввода модального окна

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,22 @@
 const profileEditButton = document.querySelector('.edit-button');
 const popupEditProfileSection = document.querySelector('.popup');
 const popupEditProfileCloseButton = popupEditProfileSection.querySelector('.popup__close-button');
+const popupEditProfileInfoInputList = popupEditProfileSection.querySelectorAll('.popup__info');
+let profileName = document.querySelector('.profile__name');
+let profileDescription = document.querySelector('.profile__description');
 
 function openPopupEditProfile() {
   popupEditProfileSection.classList.add('popup_opened');
+  autofillPopupEditProfileInputs();
 }
 
 function closePopupEditProfile() {
   popupEditProfileSection.classList.remove('popup_opened');
+}
+
+function autofillPopupEditProfileInputs() {
+  popupEditProfileInfoInputList[0].value = profileName.textContent;
+  popupEditProfileInfoInputList[1].value = profileDescription.textContent;
 }
 
 profileEditButton.addEventListener('click', openPopupEditProfile);
